55a2d18ee300855f09aecaa392a89a4b3cd9fbd8
ozcanp <63959795+ozcanp@users.noreply.github.com>
2021-05-01 17:08:04 +0300

    Add files via upload

---
+++ DijitalSaat/DijitalSaat/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace DijitalSaat
+{
+    partial class D
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(D));
+            this.timerZaman = new System.Windows.Forms.Timer(this.components);
+            this.labelYear = new System.Windows.Forms.Label();
+            this.labelMonth = new System.Windows.Forms.Label();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.labelZaman = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerZaman
+            // 
+            this.timerZaman.Enabled = true;
+            this.timerZaman.Interval = 1000;
+            this.timerZaman.Tick += new System.EventHandler(this.timerZaman_Tick);
+            // 
+            // labelYear
+            // 
+            this.labelYear.AutoSize = true;
+            this.labelYear.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelYear.Location = new System.Drawing.Point(12, 325);
+            this.labelYear.Name = "labelYear";
+            this.labelYear.Size = new System.Drawing.Size(66, 21);
+            this.labelYear.TabIndex = 1;
+            this.labelYear.Text = "Year";
+            this.labelYear.Click += new System.EventHandler(this.labelYıl_Click);
+            // 
+            // labelMonth
+            // 
+            this.labelMonth.AutoSize = true;
+            this.labelMonth.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMonth.Location = new System.Drawing.Point(312, 317);
+            this.labelMonth.Name = "labelMonth";
+            this.labelMonth.Size = new System.Drawing.Size(79, 29);
+            this.labelMonth.TabIndex = 2;
+            this.labelMonth.Text = "Month";
+            this.labelMonth.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelDay.Location = new System.Drawing.Point(148, 318);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(54, 29);
+            this.labelDay.TabIndex = 3;
+            this.labelDay.Text = "Day";
+            this.labelDay.Click += new System.EventHandler(this.labelGun_Click);
+            // 
+            // labelZaman
+            // 
+            this.labelZaman.AutoSize = true;
+            this.labelZaman.Font = new System.Drawing.Font("Lucida Console", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelZaman.Location = new System.Drawing.Point(0, 273);
+            this.labelZaman.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
+            this.labelZaman.Name = "labelZaman";
+            this.labelZaman.Size = new System.Drawing.Size(134, 32);
+            this.labelZaman.TabIndex = 0;
+            this.labelZaman.Text = "label1";
+            this.labelZaman.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-15, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(441, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // D
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 379);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.labelMonth);
+            this.Controls.Add(this.labelYear);
+            this.Controls.Add(this.labelZaman);
+            this.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "D";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DİGİTAL CLOCK ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerZaman;
+        private System.Windows.Forms.Label labelYear;
+        private System.Windows.Forms.Label labelMonth;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.Label labelZaman;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ DijitalSaat/DijitalSaat/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DijitalSaat
+{
+    public partial class D : Form
+    {
+        
+
+        public D()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerZaman_Tick(object sender, EventArgs e)
+        {
+            DateTime zaman = DateTime.Now; // Şimdiki Zaman
+            labelZaman.Text = zaman.ToString();
+            labelYear.Text = zaman.Year.ToString();
+            labelMonth.Text = CultureInfo.GetCultureInfo("en-US").DateTimeFormat.MonthNames[zaman.Month - 1];
+            labelDay.Text = CultureInfo.GetCultureInfo("en-US").DateTimeFormat.DayNames[(int)zaman.DayOfWeek];
+
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelYıl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelGun_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
